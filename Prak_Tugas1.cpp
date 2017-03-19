9c0eb730ba763d43502b3bd45b8ac7a404ceb15c
FRONT13R <FRONT13R@users.noreply.github.com>
2017-03-20 01:59:36 +0700

    Create Prak_Tugas1.cpp

---
+++ Prak_Tugas1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class Book{
+  //atribut buku NaTaJuKoPen
+  string Penulis;
+  int Tahun;
+  string Judul;
+  string Kota;
+  string Penerbit;
+  int halaman;
