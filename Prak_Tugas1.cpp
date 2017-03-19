e75b59cd3fc6b98d3657d7ec6fe3221a2888eb9c
FRONT13R <FRONT13R@users.noreply.github.com>
2017-03-20 02:06:20 +0700

    Update Prak_Tugas1.cpp

--- Prak_Tugas1.cpp
+++ Prak_Tugas1.cpp
@@ -9,3 +9,18 @@ class Book{
   string Kota;
   string Penerbit;
   int halaman;
+
+public:
+  //method
+  void setAttribute(string na, int ta, string ju, string ko, string pen, int hal);
+    this->Penulis = na;
+    this->Tahun = ta;
+    this->Judul = ju;
+    this->Kota = ko;
+    this->Penerbit = pen;
+    this->Halaman = hal;
+  
+  void showAttribute();
+  
+  
+};
