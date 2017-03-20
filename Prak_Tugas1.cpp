d0be3f4acaa969d0a1223e9bdcc6bfb78675f849
FRONT13R <FRONT13R@users.noreply.github.com>
2017-03-20 21:37:50 +0700

    Update Prak_Tugas1.cpp

--- Prak_Tugas1.cpp
+++ Prak_Tugas1.cpp
@@ -23,7 +23,7 @@ public:
         this->Penerbit = pen;
         this->Halaman = hal;
     }
-  
+
     void showAtr(){
         cout << "\n  Indeks buku ke-" << Indeks << endl; Sleep(jeda/8);
         cout << "\tPenulis  : " << this->Penulis << endl; Sleep(jeda/7);
@@ -38,11 +38,11 @@ public:
 
 void initTek(){ //kategori Teknologi
     Book Tek[5];
-    Tek[0].setAtr(1, "Penulis0", 2017, "Judul0", "Kota0", "Penerbit0", 0);
-    Tek[1].setAtr(2, "Penulis1", 2017, "Judul1", "Kota1", "Penerbit1", 1);
-    Tek[2].setAtr(3, "Penulis2", 2017, "Judul2", "Kota2", "Penerbit2", 2);
-    Tek[3].setAtr(4, "Penulis3", 2017, "Judul3", "Kota3", "Penerbit3", 3);
-    Tek[4].setAtr(5, "Penulis4", 2017, "Judul4", "Kota4", "Penerbit4", 4);
+    Tek[0].setAtr(1, "Dr. Deni Darmawan, S.Pd., M.Si", 2011, "Teknologi Pembelajaran", "Jakarta", "Remaja Rosdakarya", 112);
+    Tek[1].setAtr(2, "Murhada. S.Kom., M.M.", 2011, "Pengantar Teknologi Informasi", "Jakarta", "Mida Karya", 262);
+    Tek[2].setAtr(3, "Herry Purnomo", 2014, "Pengenalan Informatika Teknik", "Makassar", "Andi", 212);
+    Tek[3].setAtr(4, "Agung Sudjatmiko", 2012, "Teknologi sebagai Tradisi", "Medan", "Deepublish", 92);
+    Tek[4].setAtr(5, "Farhanitrate", 2008, "Organisasi dan Arsitektur Komputer", "Surabaya", "Sinamita", 319);
     cout << "Kategori Teknologi :";
     for (int i=0 ; i<5 ; i++){
         Tek[i].showAtr();
@@ -51,17 +51,17 @@ void initTek(){ //kategori Teknologi
 
 void initFil(){ //kategori Filsafat
     Book Fil[5];
-    Fil[0].setAtr(1, "Penulis0", 2017, "Judul0", "Kota0", "Penerbit0", 0);
-    Fil[1].setAtr(2, "Penulis1", 2017, "Judul1", "Kota1", "Penerbit1", 1);
-    Fil[2].setAtr(3, "Penulis2", 2017, "Judul2", "Kota2", "Penerbit2", 2);
-    Fil[3].setAtr(4, "Penulis3", 2017, "Judul3", "Kota3", "Penerbit3", 3);
-    Fil[4].setAtr(5, "Penulis4", 2017, "Judul4", "Kota4", "Penerbit4", 4);
+    Fil[0].setAtr(1, "Dr. Stephen Palmquis", 2014, "Pohon Filsafat", "Kediri", "Wedatama Widya Sastra", 120);
+    Fil[1].setAtr(2, "Agung Suganjar", 2003, "Filsafat Hukum", "Medan", "Digitama Karya", 140);
+    Fil[2].setAtr(3, "Reza Ahmadi", 2010, "Filsafat Politik untuk Indonesia", "Jakarta", "Gading Sastra", 143);
+    Fil[3].setAtr(4, "Zainal Abidin", 2005, "Filsafat Manusia", "Semarang", "Katawama", 201);
+    Fil[4].setAtr(5, "Kemal Zainudin", 2011, "Filsafat Pra-skolastik", "Malang", "Gatingka", 94);
     cout << "\nKategori Filsafat :";
     for (int i=0 ; i<5 ; i++){
         Fil[i].showAtr();
     }
 }
-  
+
 void initSej(){ //kategori Sejarah
     Book Sej[5];
     Sej[0].setAtr(1, "Penulis0", 2017, "Judul0", "Kota0", "Penerbit0", 0);
@@ -74,7 +74,7 @@ void initSej(){ //kategori Sejarah
         Sej[i].showAtr();
     }
 }
-  
+
 void initAga(){ //kategori Agama
     Book Aga[5];
     Aga[0].setAtr(1, "Penulis0", 2017, "Judul0", "Kota0", "Penerbit0", 0);
@@ -87,7 +87,7 @@ void initAga(){ //kategori Agama
         Aga[i].showAtr();
     }
 }
-  
+
 void initPsi(){ //kategori Psikologi
     Book Psi[5];
     Psi[0].setAtr(1, "Penulis0", 2017, "Judul0", "Kota0", "Penerbit0", 0);
@@ -100,7 +100,7 @@ void initPsi(){ //kategori Psikologi
         Psi[i].showAtr();
     }
 }
-  
+
 void initPol(){ //kategori Politik
     Book Pol[5];
     Pol[0].setAtr(1, "Penulis0", 2017, "Judul0", "Kota0", "Penerbit0", 0);
@@ -113,7 +113,7 @@ void initPol(){ //kategori Politik
         Pol[i].showAtr();
     }
 }
-  
+
 void initFik(){ //kategori Fiksi
     Book Fik[5];
     Fik[0].setAtr(1, "Penulis0", 2017, "Judul0", "Kota0", "Penerbit0", 0);
@@ -130,7 +130,7 @@ void initFik(){ //kategori Fiksi
 void intro(){
     cout << "Daftar Koleksi Buku Perpustakaan X\n" << endl;
 }
-    
+
 int main(){
     intro();
     initTek();
@@ -140,6 +140,6 @@ int main(){
     initPsi();
     initPol();
     initFik();
-  
+
     return 0;
 }
