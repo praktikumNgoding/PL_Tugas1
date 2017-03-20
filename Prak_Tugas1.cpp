bde5e7aa9cc014a25bb1f77684012b8d3473187f
FRONT13R <FRONT13R@users.noreply.github.com>
2017-03-20 09:11:18 +0700

    Update Prak_Tugas1.cpp

--- Prak_Tugas1.cpp
+++ Prak_Tugas1.cpp
@@ -12,15 +12,36 @@ class Book{
 
 public:
   //method
-  void setAttribute(string na, int ta, string ju, string ko, string pen, int hal);
+  void setAtr(string na, int ta, string ju, string ko, string pen, int hal){
     this->Penulis = na;
     this->Tahun = ta;
     this->Judul = ju;
     this->Kota = ko;
     this->Penerbit = pen;
     this->Halaman = hal;
+  }
   
-  void showAttribute();
+  void showAtr(){
   
-  
+  }
 };
+
+void initTek(){
+  Book Tek[5];
+  Tek[0].setAtr("Penulis0", 2017, "Judul0", "Kota0", "Penerbit0", 0);
+  Tek[1].setAtr("Penulis1", 2017, "Judul1", "Kota1", "Penerbit1", 1);
+  Tek[2].setAtr("Penulis2", 2017, "Judul2", "Kota2", "Penerbit2", 2);
+  Tek[3].setAtr("Penulis3", 2017, "Judul3", "Kota3", "Penerbit3", 3);
+  Tek[4].setAtr("Penulis4", 2017, "Judul4", "Kota4", "Penerbit4", 4);
+  cout << "Kategori Teknologi :";
+  for (int i=0 ; i<5 ; i++){
+    Tek[i].showAtr(i);
+  }
+  
+  
+}
+
+int main(){
+ 
+  return 0;
+}
